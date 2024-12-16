3b6bad48b34d1b30aa1d8c3280e21b4b3554fa31
Edd Hurst <eddhurst@users.noreply.github.com>
2024-12-16 08:49:47 +0000

    feat(AoC): day 14 pt 2 broken changing laptops

--- src/14/input.ts
+++ src/14/input.ts
@@ -5,7 +5,7 @@ import { DEBUG } from "./debug";
 import { logResult } from "../utils/log";
 import { prompt } from "./prompts/14.prompt";
 
-const { robots, map } = parseInput(prompt);
+const { robots, map } = parseInput(basic);
 
 const debug = [
   {
@@ -14,15 +14,24 @@ const debug = [
   },
 ];
 
-const WAIT = 100;
+const WAIT = 5;
 
-const finalRobots = robots.reduce((endPositions, robot) => {
-  const { startingPosition, velocity } = robot;
+const finalRobots = [] as Position[][];
+const quadrants = { q1: 0, q2: 0, q3: 0, q4: 0 };
 
-  let newPosition = { ...startingPosition };
-  if (DEBUG) console.info(robot);
-  if (DEBUG) console.info(`start at:`, newPosition);
-  for (let i = 0; i < WAIT; i++) {
+for (let s = 0; s < WAIT; s++) {
+  finalRobots.push([]);
+
+  for (let r = 0; r < robots.length; r++) {
+    const { velocity } = robots[r];
+
+    const startingPosition =
+      s === 0 ? robots[r].startingPosition : finalRobots[s - 1][r];
+
+    let newPosition = { ...startingPosition };
+    if (DEBUG) console.info(robots[r]);
+    if (DEBUG) console.info(`start at:`, newPosition);
+
     let moveHorizontal = newPosition.col + velocity.horizontal;
     if (moveHorizontal < 0) {
       moveHorizontal += map.width + 1;
@@ -40,40 +49,48 @@ const finalRobots = robots.reduce((endPositions, robot) => {
     newPosition = { col: moveHorizontal, row: moveVertical };
 
     if (DEBUG) console.info(` move to:`, newPosition);
+
+    if (
+      newPosition.col === map.middleCol ||
+      newPosition.row === map.middleRow
+    ) {
+      continue;
+    }
+
+    finalRobots[s].push(newPosition);
   }
 
-  if (newPosition.col === map.middleCol || newPosition.row === map.middleRow) {
-    return endPositions;
+  if (s === WAIT - 1) {
+    finalRobots[s].forEach((robot) => {
+      if (robot.col < map.middleCol && robot.row < map.middleRow) {
+        console.info(`[${robot.row}, ${robot.col}]: q1`);
+        quadrants.q1 += 1;
+      }
+
+      if (robot.col > map.middleCol && robot.row < map.middleRow) {
+        console.info(`[${robot.row}, ${robot.col}]: q2`);
+        quadrants.q2 += 1;
+      }
+
+      if (robot.col < map.middleCol && robot.row > map.middleRow) {
+        console.info(`[${robot.row}, ${robot.col}]: q3`);
+        quadrants.q3 += 1;
+      }
+
+      if (robot.col > map.middleCol && robot.row > map.middleRow) {
+        console.info(`[${robot.row}, ${robot.col}]: q4`);
+        quadrants.q4 += 1;
+      }
+    });
   }
+}
 
-  return [...endPositions, newPosition];
-}, [] as Position[]);
+console.info(finalRobots);
 
-const quadrants = finalRobots.reduce(
-  (quadrants, robot) => {
-    if (robot.col < map.middleCol && robot.row < map.middleRow) {
-      console.info(`[${robot.row}, ${robot.col}]: q1`);
-      return { ...quadrants, q1: quadrants.q1 + 1 };
-    }
-
-    if (robot.col > map.middleCol && robot.row < map.middleRow) {
-      console.info(`[${robot.row}, ${robot.col}]: q2`);
-      return { ...quadrants, q2: quadrants.q2 + 1 };
-    }
-
-    if (robot.col < map.middleCol && robot.row > map.middleRow) {
-      console.info(`[${robot.row}, ${robot.col}]: q3`);
-      return { ...quadrants, q3: quadrants.q3 + 1 };
-    }
-
-    if (robot.col > map.middleCol && robot.row > map.middleRow) {
-      console.info(`[${robot.row}, ${robot.col}]: q4`);
-      return { ...quadrants, q4: quadrants.q4 + 1 };
-    }
-
-    return quadrants;
-  },
-  { q1: 0, q2: 0, q3: 0, q4: 0 },
-);
+//
+// const finalRobots = robots.reduce(
+//   (endPositions, robot) => {},
+//   [] as Position[],
+// );
 
 logResult(quadrants.q1 * quadrants.q2 * quadrants.q3 * quadrants.q4);
